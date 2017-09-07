5943b7cfd0056f59c29a08ad33b782fef8012ec3
CodyHenrichsen-CTEC <cody.henrichsen@canyonsdistrict.org>
2017-09-07 13:52:29 -0600

    Learned about using a Scanner object.

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,5 +1,8 @@
 package stupid.controller;
 
+import java.util.Scanner;
+
+
 /**
  * 
  * @author Cody Henrichsen
@@ -11,5 +14,17 @@ public class StupidController
 	public void start()
 	{
 		System.out.println("I get to run 26.2 miles on Saturday morning");
+		
+		testScanner();
 	}
+	
+	private void testScanner()
+	{
+		Scanner firstScanner;
+		firstScanner = new Scanner(System.in);
+		System.out.println("What is your favorite food?");
+		String answer = firstScanner.nextLine();
+		System.out.println("Oh cool, you like " + answer);
+	}
+	
 }
